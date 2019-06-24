8af3d7cf0c1ccbf5ff22945134f9790320bc08ff
lfrz74 <anonimus47@hotmail.com>
2019-06-23 20:06:00 -0500

    2019-06-23 20:05

--- app-gallery/back-end/server/controllers/usuarios.js
+++ app-gallery/back-end/server/controllers/usuarios.js
@@ -10,6 +10,28 @@ function crearUsuario(req,res){
     })
 }
 
+function login(req,res){
+    usuarios.findOne({
+        where:{
+            usuario:req.body.usuario,
+            password:req.body.password
+        }
+    })
+    .then(usuario => {
+        if (usuario){
+            res.status(200).send({usuario});
+        }
+        else{
+            res.status(200).send({message:'Usuario no encontrado..!'});
+        }
+        
+        
+    })
+    .catch(err=>{
+        res.status(500).send({message:'Ocurrió un error al buscar el suaurio'});
+    })
+}
 module.exports={
-    crearUsuario
+    crearUsuario,
+    login
 }
--- app-gallery/back-end/server/routes/usuarios.js
+++ app-gallery/back-end/server/routes/usuarios.js
@@ -2,4 +2,5 @@ const usuariosController = require('../controllers').usuarios;
 
 module.exports = (app) => {
     app.post('/api/usuario', usuariosController.crearUsuario);
+    app.post('/api/login', usuariosController.login);
 }
