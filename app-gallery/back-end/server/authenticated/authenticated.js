2f3d416638f35d9183d51fbe5f9879ebc22cb86f
lfrz74 <anonimus47@hotmail.com>
2019-06-29 22:50:14 -0500

    29/Jun/2019 22:50

---
+++ app-gallery/back-end/server/authenticated/authenticated.js
@@ -0,0 +1,23 @@
+var nJwt = require('njwt');
+var config = require('../config/config');
+var secret = config.token_secret;
+
+//middleware: bloque de código q se ejecuta entre cada petición del usuario
+function auth(req, res, next){
+    if(!req.headers.authorization){
+        return res.status(403).send({message:'La petición no tiene la cabecera de autenticación..!'});
+    }
+
+    var token = req.headers.authorization.replace(/['"]+/g,'');
+    var payload = nJwt.verify(token, secret, (err, verifiedJwt) => {
+        if (err){
+            return res.status(401).send({message: 'Acceso no autorizado..!'});
+        }else{
+            next();
+        }
+    });
+}
+
+module.exports = {
+    auth
+}
