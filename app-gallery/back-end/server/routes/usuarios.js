66c21a90f325e430ffa8aa9afd791092527000e7
lfrz <anonimus47@hotmail.com>
2019-07-14 19:37:52 -0500

    Eliminar objetos que subieron mal

--- app-gallery/back-end/server/routes/usuarios.js
+++
@@ -1,8 +0,0 @@
-const usuariosController = require('../controllers').usuarios;
-const md_auth = require('../authenticated/authenticated');
-
-module.exports = (app) => {
-    app.post('/api/usuario', md_auth.auth, usuariosController.crearUsuario);
-    app.post('/api/login', usuariosController.login);
-    app.get('/api/usuarios', md_auth.auth, usuariosController.consultarUsuarios);
-}
